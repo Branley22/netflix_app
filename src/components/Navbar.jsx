ab00f255e480049a694d603b860b06fcc8ad716c
Branley Minaya <bminaya23@gmail.com>
2022-09-07 21:35:15 -0400

    login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,38 @@
+import { async } from "@firebase/util";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
+  const {user, logOut} = UserAuth()
+  const navigate = useNavigate();
+  // console.log(user.email)
+
+  const handleLogout = async () => {
+    try {
+      await logOut()
+      navigate("/")
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className="flex items-center justify-between p-4 w-full absolute z-[100]">
       <Link to="/">
         <h1 className="text-red-600 text-4xl font-bold cursor-pointer">NETFLIX</h1>
       </Link>
-      <div>
+
+      {user?.email ? (
+        <div>
+        <Link to="/account">
+          <button className="text-white pr-4">Account</button>
+        </Link>
+          <button onClick={handleLogout}
+          className="bg-red-600 text-white px-6 py-2 rounded cursor-pointer">Logout</button>
+      </div>
+      ) : (
+        <div>
         <Link to="/login">
           <button className="text-white pr-4">Sign In</button>
         </Link>
@@ -15,6 +40,7 @@ const Navbar = () => {
           <button className="bg-red-600 text-white px-6 py-2 rounded cursor-pointer">Sign Up</button>
         </Link>
       </div>
+      )}
     </div>
   );
 };
